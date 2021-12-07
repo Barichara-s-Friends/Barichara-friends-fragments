5b1477f73df1e136766cfe10aed7c2e6af8ad2af
christianrruiz <88356596+christianrruiz@users.noreply.github.com>
2021-12-07 12:21:11 -0500

    json desde servidor y mini  mapa funcional

--- baricharafriends/src/main/java/com/edw88/baricharafriends/maps/MapsFragment.kt
+++ baricharafriends/src/main/java/com/edw88/baricharafriends/maps/MapsFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.edw88.baricharafriends.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
